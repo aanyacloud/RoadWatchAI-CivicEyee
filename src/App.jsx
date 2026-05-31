e7d84c92ed2f9d033e4e1d43cbc8cc284eb11bbe
Aanya <aanyac94@gmail.com>
2026-05-31 22:40:01 +0530

    Removed firebase secrets

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import ReportIssue from './pages/ReportIssue'
-<<<<<<< HEAD
 import ContractorDetails from './pages/ContractorDetails'
-=======
->>>>>>> f6a98ec8f5f6ce3550d8d554cc80c698ab28e880
 
 export default function App() {
   return (
@@ -17,11 +14,7 @@ export default function App() {
 
         <Route path="/report" element={<ReportIssue />} />
 
-<<<<<<< HEAD
         <Route path="/contractors" element={<ContractorDetails />} />
-
-=======
->>>>>>> f6a98ec8f5f6ce3550d8d554cc80c698ab28e880
       </Routes>
 
     </BrowserRouter>
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZ3b_lAhBt-ug-tgekCOcOIii5GCZI-xg",
-  authDomain: "roadwatch-1e941.firebaseapp.com",
-  projectId: "roadwatch-1e941",
-  storageBucket: "roadwatch-1e941.firebasestorage.app",
-  messagingSenderId: "248845884665",
-  appId: "1:248845884665:web:ecfbf53871af581d471628",
-  measurementId: "G-KWV10G1QT9"
+  apiKey:
+  authDomain: 
+  projectId: 
+  storageBucket:
+  messagingSenderId:
+  appId: 
+  measurementId:
 }
 
 const app = initializeApp(firebaseConfig)
